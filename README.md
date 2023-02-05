0b494fa8f94d2063903786e915567c5a94c6bc56
victoriaajuwon <122409743+victoriaajuwon@users.noreply.github.com>
2023-02-05 15:25:49 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Test-Automation-UniversityTestNG
+This is to save all codes written for the TestNG tutorial on Test Automation University
---
+++ TestAutomationUniversity/src/com/testautomationu/chapter3annotations/DemoAnnotation.java
@@ -0,0 +1,17 @@
+package com.testautomationu.chapter3annotations;
+
+import org.testng.annotations.BeforeMethod;
+
+public class DemoAnnotation {
+	
+	@BeforeMethod
+	public void setUp() {
+		
+	}
+
+	/*public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}*/
+
+}
